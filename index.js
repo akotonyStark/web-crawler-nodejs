a2db4179563d712017f7d64944869f8c82775d47
Augustine Akoto Larbi-Ampofo <alarbiampofo@gmail.com>
2022-05-06 13:41:55 +0000

    recursively call crawl funtion to crawl sub urls

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import fetch from 'node-fetch'
 import fs from 'fs'
 
 let results = []
+let ROOTURL = 'http://127.0.0.1:5500/'
 //web crawling function
 const webCrawl = async (url) => {
   const response = await fetch(url)
@@ -13,14 +14,18 @@ const webCrawl = async (url) => {
     .map((i, link) => link.attribs.href)
     .get()
 
+  const imageURLS = $('img')
+    .map((i, imgLink) => imgLink.attribs.src)
+    .get()
+
   links.forEach((link, index) => {
-    // console.log('crawling:', link)
-
-    const imageURLS = $('img')
-      .map((i, link) => link.attribs.src)
-      .get()
-
-    const obj = { imageUrl: imageURLS, sourceUrl: link, depth: index }
+    console.log('crawling:', link)
+    webCrawl(convertToURL(link))
+    const obj = {
+      imageUrl: imageURLS,
+      sourceUrl: `${ROOTURL}${link}`,
+      depth: index,
+    }
     results.push(obj)
   })
 
@@ -33,4 +38,21 @@ const webCrawl = async (url) => {
   })
 }
 
-webCrawl('https://www.geeksforgeeks.org/')
+//converting to valid URL
+const convertToURL = (link) => {
+  if (link.includes('http')) {
+    return link
+  } else if (
+    link.startsWith('/') ||
+    link.startsWith('~') ||
+    link.startsWith('#')
+  ) {
+    return `${ROOTURL}${link.substring(1)}`
+  } else if (link.startsWith('.')) {
+    return `${ROOTURL}${link.substring(2)}`
+  } else {
+    return `${ROOTURL}${link}`
+  }
+}
+
+webCrawl(ROOTURL)
